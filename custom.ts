7b0bf8d4b18577ee472edb8bf661a1a9be96178a
10-biter <ep.student@hotmail.com>
2025-12-06 13:17:43 +0300

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -1,26 +1,15 @@
-
-/**
-* Use this file to define custom functions and blocks.
-* Read more at https://arcade.makecode.com/blocks/custom
-*/
-
-
-
-/**
- * Custom blocks
- */
-//% weight=100 color=#1c1f78 icon=""
+//% color=#1c1f78 icon=""
 namespace spriteSleep {
-    /**
-     * TODO: describe your function here
-     * @param n describe parameter here, eg: 5
-     * @param s describe parameter here, eg: "Hello"
-     * @param e describe parameter here
-     */
     //% block
     export function sleep(sprite:Sprite): void {
         controller.moveSprite(sprite, 0, 0)
         sprite.setFlag(SpriteFlag.Ghost, true)
+    }
+    
 
+
+    export function wakeUp(sprite:Sprite): void {
+        controller.moveSprite(sprite)
+        sprite.setFlag(SpriteFlag.Ghost, false)
     }
 }
